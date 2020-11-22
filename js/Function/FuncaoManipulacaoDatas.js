26da47def9db51a76f4b9d10efbdaf8f01fe01d1
daniel camucatto <daniel.camucatto@gmail.com>
2020-11-21 23:51:20 -0300

    add funcoes nativas

---
+++ js/Function/FuncaoManipulacaoDatas.js
@@ -0,0 +1,10 @@
+/*
+    FUNCAO PARA MANIPULACAO DE DATAS
+        para manipular datas em javaScript é necessario instanciar um novo objeto 
+*/
+
+const data = new Date(); // instanciando uma nova data 
+
+console.log(data.getDate()); // pega o dia baseado no sistema operacional 
+console.log(data.getMonth() + 1) // pega o mês atual 
+console.log(data.getFullYear()); // pega o ano atual 
